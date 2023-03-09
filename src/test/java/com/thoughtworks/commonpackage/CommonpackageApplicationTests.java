e290813d9a0cb79bb4c6166125762ce3f80bdf7c
RagulTW <ragul.r@thoughtworks.com>
2023-03-09 14:37:14 +0530

    [Ragul | Harsha] Add. workflow to publish in registry

---
+++ src/test/java/com/thoughtworks/commonpackage/CommonpackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.commonpackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonpackageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
