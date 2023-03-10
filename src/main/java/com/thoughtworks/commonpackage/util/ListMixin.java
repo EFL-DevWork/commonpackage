ec7666fb74e1b652a04355ac1192c44d26e08010
RagulTW <ragul.r@thoughtworks.com>
2023-03-10 15:16:54 +0530

    [Ragul | Harsha] Add. ListMixin

---
+++ src/main/java/com/thoughtworks/commonpackage/util/ListMixin.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.commonpackage.util;
+
+import java.util.List;
+
+public interface ListMixin<T> extends List<T> {
+
+    List<T> getRecords();
+
+    @Override
+    default int size() { return getRecords().size();}
+
+    @Override
+    default boolean isEmpty() {return getRecords().isEmpty();}
+}
